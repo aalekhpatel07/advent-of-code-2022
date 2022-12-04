2fa645961cf6dcc51f5e5e4901dc6b03b6b064b4
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-12-03 22:05:34 -0600

    Add solution for day 1.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- aoc/cmd/day-01/main.go
+++ aoc/cmd/day-01/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"aoc/internal"
+	"aoc/pkg/input"
+	"aoc/pkg/structs"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello!")
+	inputs := input.GetInputs(1, 2022)
+	part1Answer := Part1(inputs)
+	fmt.Println(fmt.Sprintf("Part 1: %d", part1Answer))
+	part2Answer := Part2(inputs)
+	fmt.Println(fmt.Sprintf("Part 2: %d", part2Answer))
+}
+
+func Part1(inputs []structs.Group) int64 {
+	sums := make([]int64, len(inputs))
+
+	for idx, group := range inputs {
+		sums[idx] = sumContents(group.Contents)
+	}
+	result, _ := internal.Max(sums)
+	return result
+}
+
+func Part2(inputs []structs.Group) int64 {
+	sums := make([]int64, len(inputs))
+
+	for idx, group := range inputs {
+		sums[idx] = sumContents(group.Contents)
+	}
+	sort.Slice(sums, func(i, j int) bool {
+		return sums[i] > sums[j]
+	})
+	return sums[0] + sums[1] + sums[2]
+}
+
+func sumContents(contents string) int64 {
+	stringlyNumbers := strings.Split(contents, "\n")
+	var sum int64 = 0
+	for _, number := range stringlyNumbers {
+		val, _ := strconv.ParseInt(number, 10, 0)
+		sum += val
+	}
+	return sum
 }
--- aoc/go.mod
+++ aoc/go.mod
@@ -1,3 +1,8 @@
 module aoc
 
 go 1.18
+
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/yanatan16/itertools v0.0.0-20160513161737-afd1891e0c4f // indirect
+)
