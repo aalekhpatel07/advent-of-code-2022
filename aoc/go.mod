f847f6e27e7a2462e8ced0440f6efd91817bd236
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-12-05 00:11:21 -0600

    Add solution for day 5.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- aoc/go.mod
+++ aoc/go.mod
@@ -6,6 +6,7 @@ require github.com/spf13/cobra v1.6.1
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/niemeyer/golang v0.0.0-20110826170342-f8c0f811cb19 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/exp v0.0.0-20221204150635-6dcec336b2bb // indirect
 )
--- aoc/pkg/year_2022/day-05.go
+++ aoc/pkg/year_2022/day-05.go
@@ -1,60 +1,140 @@
 package year_2022
 
 import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/niemeyer/golang/src/pkg/container/vector"
+
 	"aoc/pkg/structs"
 )
 
 type Day05 struct {
 }
 
+type Move struct {
+	Count  int
+	Source int
+	Target int
+}
+
+var regexCompiled = regexp.MustCompile(`move (?P<Count>\d+) from (?P<Source>\d+) to (?P<Target>\d+)`)
+
+func NewMove(s string) *Move {
+	if s == "" {
+		return nil
+	}
+	match := regexCompiled.FindStringSubmatch(s)
+	count, _ := strconv.Atoi(match[1])
+	source, _ := strconv.Atoi(match[2])
+	target, _ := strconv.Atoi(match[3])
+	return &Move{
+		Count:  count,
+		Source: source,
+		Target: target,
+	}
+}
+
+func (move *Move) apply(stacks []vector.IntVector) []vector.IntVector {
+
+	sourceVector := stacks[move.Source-1]
+	targetVector := stacks[move.Target-1]
+
+	for i := 0; i < move.Count; i += 1 {
+		targetVector.Push(sourceVector.Pop())
+	}
+
+	stacks[move.Source-1] = sourceVector
+	stacks[move.Target-1] = targetVector
+
+	return stacks
+}
+
+func (move *Move) applyMultipleCrates(stacks []vector.IntVector) []vector.IntVector {
+
+	sourceVector := stacks[move.Source-1]
+	targetVector := stacks[move.Target-1]
+
+	targetVector.AppendVector(sourceVector.Slice(sourceVector.Len()-move.Count, sourceVector.Len()))
+
+	for i := 0; i < move.Count; i++ {
+		sourceVector.Pop()
+	}
+
+	stacks[move.Source-1] = sourceVector
+	stacks[move.Target-1] = targetVector
+
+	return stacks
+}
+
+func GetStacks(inputs []structs.Group) []vector.IntVector {
+	// I'm not gonna write a parser for this weird structure.
+	// The stack data is hardcoded in this solution.
+
+	//			[G]         [D]     [Q]
+	//	[P]     [T]         [L] [M] [Z]
+	//	[Z] [Z] [C]         [Z] [G] [W]
+	//	[M] [B] [F]         [P] [C] [H] [N]
+	//	[T] [S] [R]     [H] [W] [R] [L] [W]
+	//	[R] [T] [Q] [Z] [R] [S] [Z] [F] [P]
+	//	[C] [N] [H] [R] [N] [H] [D] [J] [Q]
+	//	[N] [D] [M] [G] [Z] [F] [W] [S] [S]
+	//	 1   2   3   4   5   6   7   8   9
+
+	stacks := make([]vector.IntVector, 9)
+
+	stacks[0] = vector.IntVector{'N', 'C', 'R', 'T', 'M', 'Z', 'P'}
+	stacks[1] = vector.IntVector{'D', 'N', 'T', 'S', 'B', 'Z'}
+	stacks[2] = vector.IntVector{'M', 'H', 'Q', 'R', 'F', 'C', 'T', 'G'}
+	stacks[3] = vector.IntVector{'G', 'R', 'Z'}
+	stacks[4] = vector.IntVector{'Z', 'N', 'R', 'H'}
+	stacks[5] = vector.IntVector{'F', 'H', 'S', 'W', 'P', 'Z', 'L', 'D'}
+	stacks[6] = vector.IntVector{'W', 'D', 'Z', 'R', 'C', 'G', 'M'}
+	stacks[7] = vector.IntVector{'S', 'J', 'F', 'L', 'H', 'W', 'Z', 'Q'}
+	stacks[8] = vector.IntVector{'S', 'Q', 'P', 'W', 'N'}
+	return stacks
+}
+
 func (d *Day05) Part1(inputs []structs.Group) string {
 
-	//contents := inputs[0].Contents
-	//rounds := strings.Split(contents, "\n")
-	//var count int32 = 0
-	//
-	//for _, round := range rounds {
-	//	if round == "" {
-	//		break
-	//	}
-	//	sections := strings.Split(round, ",")
-	//	section1 := FromStr(sections[0])
-	//	section2 := FromStr(sections[1])
-	//
-	//	pair := WorkPair{
-	//		Section1: section1,
-	//		Section2: section2,
-	//	}
-	//	if pair.overlapsCompletely() {
-	//		count += 1
-	//	}
-	//}
-	//return fmt.Sprintf("%d", count)
-	return ""
+	stacks := GetStacks(inputs)
+
+	moves := inputs[1]
+	for _, moveRaw := range strings.Split(moves.Contents, "\n") {
+		move := NewMove(moveRaw)
+		if move == nil {
+			break
+		}
+		stacks = move.apply(stacks)
+	}
+
+	result := make([]string, len(stacks))
+	for idx, stack := range stacks {
+		result[idx] = fmt.Sprintf("%c", stack.Last())
+	}
+
+	return strings.Join(result, "")
 }
 
 func (d *Day05) Part2(inputs []structs.Group) string {
 
-	//contents := inputs[0].Contents
-	//rounds := strings.Split(contents, "\n")
-	//var count int32 = 0
-	//
-	//for _, round := range rounds {
-	//	if round == "" {
-	//		break
-	//	}
-	//	sections := strings.Split(round, ",")
-	//	section1 := FromStr(sections[0])
-	//	section2 := FromStr(sections[1])
-	//
-	//	pair := WorkPair{
-	//		Section1: section1,
-	//		Section2: section2,
-	//	}
-	//	if pair.overlaps() {
-	//		count += 1
-	//	}
-	//}
-	//return fmt.Sprintf("%d", count)
-	return ""
+	stacks := GetStacks(inputs)
+
+	moves := inputs[1]
+	for _, moveRaw := range strings.Split(moves.Contents, "\n") {
+		move := NewMove(moveRaw)
+		if move == nil {
+			break
+		}
+		stacks = move.applyMultipleCrates(stacks)
+	}
+
+	result := make([]string, len(stacks))
+	for idx, stack := range stacks {
+		result[idx] = fmt.Sprintf("%c", stack.Last())
+	}
+
+	return strings.Join(result, "")
 }
